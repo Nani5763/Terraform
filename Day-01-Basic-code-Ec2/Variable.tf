45078b2c38097e3b29c3bbbcbbb85beb0a53dd58
Pavan sai <pavanaws57@gmail.com>
2024-05-09 11:06:24 +0530

    updates files

---
+++ Day-01-Basic-code-Ec2/Variable.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+    type = string
+    default = "ami-07caf09b362be10b8"
+  
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+  
+}
+variable "key_name" {
+    type = string
+    default = "first-key"
+  
+}
