187e10f4b64f8ddc4c2bd51d5ba2695d8fee8e38
Pavan sai <pavanaws57@gmail.com>
2024-05-11 15:19:34 +0530

    update

--- Day-06-Data-source/Main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "Pavan" {
-    ami = data.aws_ami.amzlinux
-    instance_type = "t2.micro"
-    subnet_id = data.aws_subnet.Pavan.id
-    security_groups = [ data.aws_security_group.Pavan.id ]
-
-  
-}
