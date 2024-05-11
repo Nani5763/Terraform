0ee7b6126f0697164568dc3a9fb0475986fbe929
Pavan sai <pavanaws57@gmail.com>
2024-05-11 11:12:47 +0530

    update Outputs files

--- Day-02-Custom-Netwrok/Output.tf
+++ Day-02-Custom-Netwrok/Output.tf
@@ -7,3 +7,11 @@ output "private_ip" {
     value = aws_instance.Pavan_dev.private_ip
   
 }
+output "security_groups" {
+    value = aws_security_group.Pavan.id
+  
+}
+output "vpc_id" {
+    value = aws_vpc.Pavan.id
+  
+}
