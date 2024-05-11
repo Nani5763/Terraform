4bdf36683d34a235d964ab2f4433d73137293700
Pavan sai <pavanaws57@gmail.com>
2024-05-11 11:05:10 +0530

    update SG

--- Day-02-Custom-Netwrok/Main.tf
+++ Day-02-Custom-Netwrok/Main.tf
@@ -42,4 +42,46 @@ resource "aws_route_table_association" "Pavan" {
     
   
 }
-#Launch Ec2
+#Create Security Group
+resource "aws_security_group" "Pavan" {
+  name        = "allow_tls"
+  vpc_id      = aws_vpc.Pavan.id
+  tags = {
+    Name = "Pavan_sg"
+  }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+
+}
--- Day-02-Custom-Netwrok/ec2.tf
+++ Day-02-Custom-Netwrok/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "Pavan_dev" {
     key_name = var.key_name
     subnet_id = aws_subnet.Pavan.id
     associate_public_ip_address = true
+    security_groups = [ aws_security_group.Pavan.id ]
     tags = {
       Name = "Pavan_dev"
     }
